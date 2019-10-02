e4d17be2e8b5a491926495a2939e426a749773cd
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-10-02 09:15:06 -0300

    telas que exibirão clientes cadastrados

---
+++ src/components/Pedido/CadastroGeral.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-col cols="12">
+        <!-- Inicio do card da tela  -->
+        <v-card>
+          <v-card-title>
+            <p class="title">Clientes | Cadastros</p>
+            <v-spacer></v-spacer>
+            <p class="text-center title">Total de cadastros: 1537</p>
+            <v-spacer></v-spacer>
+            <v-btn id="BtnCadastrar" small color="primary"> <span class="white--text">Cadastrar - Alt+F1</span></v-btn>
+          </v-card-title>
+          <v-tabs v-model="tab" dark grow>
+            <v-tab>
+              Nome
+            </v-tab>
+            <v-tab>
+              Endereço
+            </v-tab>
+            <v-tab>
+              Almoxarifado
+            </v-tab>
+          </v-tabs>
+
+          <v-tabs-items v-model="tab">
+            <v-tab-item>
+              <Nome/>
+            </v-tab-item>
+            <v-tab-item>
+              <Endereco/>
+            </v-tab-item>
+            <v-tab-item>
+              <Almoxarifado/>
+            </v-tab-item>
+          </v-tabs-items>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Nome from "./Nome"
+import Endereco from "./Endereco"
+import Almoxarifado from "./Almoxarifado"
+export default {
+  components: {
+    Nome,
+    Endereco,
+    Almoxarifado,
+  },
+  data () {
+    return {
+      tab: null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+#BtnCadastrar:hover {
+color: #BDBDBD;
+}
+
+</style>
+
---
+++ src/components/Pedido/Nome.vue
@@ -0,0 +1,165 @@
+<template>
+   <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th class="text-left">CÓDIGO</th>
+          <th class="text-left">NOME</th>
+          <th class="text-left">TELEFONE</th>
+          <th class="text-left">ENDEREÇO</th>
+          <th class="text-left">RG</th>
+          <th class="text-left">REFERÊNCIA</th>
+          <th class="text-left">CEP</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in searchname" :key="item.name">
+          <td>{{ item.codigo }}</td>
+          <td><strong>{{ item.nome }}</strong></td>
+          <td>{{ item.telefone }}</td>
+          <td>{{ item.endereco }}</td>
+          <td>{{ item.rg }}</td>
+          <td>{{ item.referencia }}</td>
+          <td>{{ item.cep }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        searchname: [
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+          {
+            codigo:'00000',
+            nome: 'Patrick Lorran',
+            telefone: '+55 96 981134321',
+            endereco: 'Alameda B, Rua 19 de Maio | Nova União',
+            rg: '600543',
+            referencia: 'Próximo a Creche Mauro César',
+            cep: '68927021',
+          },
+        ],
+      }
+    },
+  }
+</script>
