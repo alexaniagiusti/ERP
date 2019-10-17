2ebf9b72afc4afe6ecb2afb80318f485dd57283a
Alexânia Giusti <alexaniagiusti@hotmail.com>
2019-10-17 11:47:44 -0300

    Formulário par cadastro de cliente

--- src/components/Cadastros/Clientes.vue
+++ src/components/Cadastros/Clientes.vue
@@ -1,3 +1,74 @@
 <template>
-    <p>Tela para cadastro de cliente, formulário</p>
+	<v-container fluid>
+		<v-row>
+			<v-col cols="12" md="12" xs="12">
+				<v-card class="pa-2">
+					<v-card-title><span class="title">Cadastrar Novo Cliente</span></v-card-title>
+					<v-row>
+						<v-col  cols="12" md="3" xs="12">
+							<v-text-field outlined label="Nome"></v-text-field>	
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="CPF"></v-text-field>	
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="RG"></v-text-field>	
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="CPNJ"></v-text-field>	
+						<v-row>
+						</v-row>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="IE"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="CEP"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Logradouro"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Número"></v-text-field>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Bairro"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Cidade"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="UF"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Referência"></v-text-field>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Telefone"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Celular 1"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="Celular 2"></v-text-field>
+						</v-col>
+						<v-col cols="12" md="3" xs="12">
+							<v-text-field outlined label="E-mail"></v-text-field>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col cols="12" md="12" xs="12">
+							<v-btn color="primary" block>Cadastrar</v-btn>
+						</v-col>
+					</v-row>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
